7e124d9956a3555f31a2b518ee810f16359e3958
rjacquier <rjacquier@insa-rouen.fr>
2017-12-18 11:09:13 +0100

    initial commit'

---
+++ src/SGBD.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SGBD {
+
+    private static String bd = "jdbc:postgresql://asi-pg.insa-rouen.fr:5432/orange-12";
+    private static String user = "grtt12";
+    private static String passwd = "grtt12";
+
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(bd, user, passwd);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+
+    public static void deconnexion() throws SQLException {
+
+        conn.close();
+    }
+
+}
