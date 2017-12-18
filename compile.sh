62f0a8baa9d75be1810facea2e66bf849f456fe6
Baert Gaetan <gbaert@asi-testing>
2017-12-18 14:18:16 +0100

    qsd

--- compile.sh
+++ compile.sh
@@ -2,3 +2,4 @@ mkdir -p classes
 javac -cp classes:gs-core-1.3/gs-core-1.3.jar -d classes/ src/application/sgbd/*.java
 javac -cp classes:gs-core-1.3/gs-core-1.3.jar -d classes/ src/application/*.java
 java -cp classes:gs-core-1.3/gs-core-1.3.jar:Driver/postgresql-42.1.4.jar application.Main
+sdfsdq
