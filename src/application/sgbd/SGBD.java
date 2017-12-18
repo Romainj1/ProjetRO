e243972907b816d680b92c66c2aaa6e4f22e5bb7
romain Jacquier <romain.jacquier@insa-rouen.fr>
2017-12-18 12:20:23 +0100

    add sql commands in comments

--- src/application/sgbd/SGBD.java
+++ src/application/sgbd/SGBD.java
@@ -27,4 +27,8 @@ public class SGBD {
         conn.close();
     }
 
+    //SELECT "nomCommune" FROM communes where "codeDepartement" = '76';
+    //\dt
+    // SELECT "nomCommune", "codeDepartement" FROM communes where "codeInsee" = '76540';
+
 }
