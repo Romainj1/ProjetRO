faa4c05a37daa2b39dd51ae078c3f3c08b0383e1
romain Jacquier <romain.jacquier@insa-rouen.fr>
2017-12-18 11:59:33 +0100

    add compile.sh

---
+++ compile.sh
@@ -0,0 +1,4 @@
+mkdir -p classes
+javac -cp classes:gs-core-1.3/gs-core-1.3.jar -d classes/ src/application/*.java
+javac -cp classes:gs-core-1.3/gs-core-1.3.jar -d classes/ src/application/sgbd/*.java
+java -cp classes:gs-core-1.3/gs-core-1.3.jar:Driver/postgresql-42.1.4.jar application.Main
