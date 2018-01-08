cf25e58f409a24565333ce65e882cdbf2a857468
romain Jacquier <romain.jacquier@insa-rouen.fr>
2018-01-08 12:13:26 +0100

    ça remarche

--- compile.sh
+++ compile.sh
@@ -1,12 +1,11 @@
 mkdir -p classes
 echo "compilation en cours ..."
 echo "compilation package ihm"
-javac -cp classes:include/*.jar -d classes/ src/ihm/*.java
+javac -cp classes:include/gs-core-1.3.jar:include/postgis.jar:include/postgresql-42.1.4.jar:include/gs-core-1.3-sources.jar -d classes/ src/ihm/*.java
 echo "compilation package sgbd"
-javac -cp classes:include/gs-core-1.3.jar:include/postgis.jar:include/postgresql-42.1.4.jar -d classes/ src/application/sgbd/*.java
+javac -cp classes:include/gs-core-1.3.jar:include/postgis.jar:include/postgresql-42.1.4.jar:include/gs-core-1.3-sources.jar -d classes/ src/application/sgbd/*.java
 echo "compilation package affichage"
-javac -cp classes:include/gs-core-1.3.jar:include/postgis.jar:include/postgresql-42.1.4.jar -d classes/ src/application/affichage/*.java
+javac -cp classes:include/gs-core-1.3.jar:include/postgis.jar:include/postgresql-42.1.4.jar:include/gs-core-1.3-sources.jar -d classes/ src/application/affichage/*.java
 echo "compilation package Main"
-javac -cp classes:include/gs-core-1.3.jar:include/postgis.jar:include/postgresql-42.1.4.jar -d classes/ src/application/*.java
+javac -cp classes:include/gs-core-1.3.jar:include/postgis.jar:include/postgresql-42.1.4.jar:include/gs-core-1.3-sources.jar -d classes/ src/application/*.java
 echo "compilation terminé ! pour lancer l'application : $ source run.sh"
-
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -cp classes:include/gs-core-1.3.jar:include/postgis.jar:include/postgresql-42.1.4.jar application.Main 
+java -cp classes:include/gs-core-1.3.jar:include/postgresql-42.1.4.jar:include/postgis.jar application.Main
